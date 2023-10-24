435cd091c092c41c2b21883b3c425e6ebba1f775
Hanson Tram <htram.dev@gmail.com>
2023-10-24 13:26:26 -0700

    Finish exercise one

---
+++ functions/simple_functions.js
@@ -0,0 +1,19 @@
+// Exercise 1
+
+function favoriteThings(){
+    console.log(`Name: Hanson`)
+    console.log(`Favorite Movie: Rush Hour`)
+    console.log(`Favorite Artist: Drake`)
+}
+
+function whyImHere(){
+    console.log("I joined this program to learn a new skill and earn an internship")
+}
+
+function favoritePlace(){
+    console.log("My favorite place to visit is vegas because it is always a fun time")
+}
+
+favoriteThings()
+whyImHere()
+favoritePlace()
